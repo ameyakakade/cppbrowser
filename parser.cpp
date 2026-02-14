f2e71f4212c4449e620c660881869d3d15ce85fa
ameyakakade <ameyakakade@gmail.com>
2026-02-14 12:21:53 +0530

    Worked on the parser Some bugs are present

--- parser.cpp
+++ parser.cpp
@@ -16,12 +16,10 @@ void htmlParser::parse(std::string input){
     int type = none;
     std::string data;
     treeNode* curr = domTree;
-    std::vector<std::string> attributes;
+    std::string attributes;
 
     for(auto letter : input){
 
-        // convert this into a parse -> set state -> then act on the state type thing
-        
         switch (letter){
 
             case '<':
@@ -39,25 +37,40 @@ void htmlParser::parse(std::string input){
                 state = readingTagEnd;
                 break;
 
+            case ' ':
+                if(state == readingTag){
+                    state = readingTagContents;
+                }
+
             default:
                 if(state == readingTagStart){
-                    // we started reading the thing
                     state = readingTag;
                 }else if(state == readingTagEnd){
                     state = outside;
                     type = textData;
                 }
-
         }
 
         switch (state){
 
             case readingTagStart:
                 // init data when we start reading smth
-                data = "";
+                if(data != ""){
+                    curr->data = data;
+                }
+                data.clear();
+                attributes.clear();
                 break;
 
             case readingTag:
+                data += letter;
+                break;
+
+            case readingTagContents:
+                attributes += letter;
+                break;
+
+            case outside:
                 data += letter;
                 break;
 
@@ -65,34 +78,15 @@ void htmlParser::parse(std::string input){
                 if(type == tag){
                     treeNode* temp = new treeNode(data, curr);
                     curr->children.emplace_back(temp);
+                    std::cout << attributes;
                     curr = temp;
                 }else if (type == endTag){
                     curr = curr->parentNode;
                 }
+                data.clear();
+                attributes.clear();
                 break;
         }
-
-        // if(letter=='<'){
-        //     // write the data to the current node
-        //     // set state to reading tag
-        //     curr->data = data;
-        //     state = reading;
-        //     word = "";
-        //     data = "";
-        // }else if(state == reading && word == "" && letter == '/'){
-        //     // if end tag is found set state to reading end and move up the dom tree
-        //     curr = curr->parentNode;
-        //     state = readingend;
-        // }else if(letter=='>' && state==reading){
-        //     // if tag is ended and we were reading a tag make a new node in the tree and move curr to it
-        //     state = outside;
-        // }else if(state==reading){
-        //     // when reading html tag
-        //     word += letter;
-        // }else if(state == outside){
-        //     // when reading data
-        //     data += letter;
-        // }
 
     }
 
@@ -101,12 +95,13 @@ void htmlParser::parse(std::string input){
 void htmlParser::traverse(treeNode* node, int level){
     std::string indent;
     for(int i=0; i<level; i++){
-        indent += '\t';
+        indent += "  ";
     }
-    std::cout << indent << node->name << node->data << std::endl;
+    std::cout << indent << node->name << node->data.size();
     for(auto property : node->attributes){
-        std::cout << property.name << " " << property.value << std::endl;
+        std::cout << " " << property.name << ":" << property.value;
     }
+    std::cout << "\n";
     for(auto child : node->children){
         traverse(child, level+1);
     }
@@ -126,10 +121,10 @@ void htmlParser::inheritCss(treeNode* node){
     // add all attributes from parent node
     for(cssProperty attribute : *parentCss){
         if(!selfCssAttributesCache.count(attribute.name)){
-            std::cout << "oo i inherit from body" << std::endl;
+            // std::cout << "oo i inherit from body" << std::endl;
             node->attributes.push_back(attribute);
         }else{
-            std::cout << "oops i already have it" << std::endl;
+            // std::cout << "oops i already have it" << std::endl;
         }
     }
 
